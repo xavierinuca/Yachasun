d235fa67d5841ac3ab9845d51290a6131142a90f
Xavier Inuca <bryanxavier95@gmail.com>
2018-06-13 07:06:23 -0500

    Cambio de imagenes y arreglo de layout o vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+//RUTAS
+import { app_routing } from './app.routes';
 
+//SERVICIOS
+import { InformacionService } from './services/informacion.service';
+import { ProductosService } from './services/productos.service';
+//COMPONENTES
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemComponent } from './components/item/item.component';
+import { SearchComponent } from './components/search/search.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotificacionesComponent } from './components/notificaciones/notificaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
+    SearchComponent,
+    NotFoundComponent,
+    NotificacionesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    InformacionService,
+    ProductosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
